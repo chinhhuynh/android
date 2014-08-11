af34d3879c6c430a07f85313235f3e65ed3c1400
chinhhuynh <chinhhuynh@chinhhuynh-macbookair1.roam.corp.google.com>
2014-08-10 21:23:29 -0700

    Add test app Activity. AS build is broken with duplicate class which needs to be fixed.

---
+++ tests/libraries/mediaplayer/app/MediaPlayerActivity.java
@@ -0,0 +1,11 @@
+package com.chinhhuynh.android.libraries.mediaplayerapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MediaPlayerActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}

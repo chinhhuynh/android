587f59b4bebc79b3a69252e12bc51dbd33a722c4
Chinh Huynh <huynhphungchinh@gmail.com>
2016-01-30 10:47:27 -0800

    Revert "Remove unused code."
    This reverts commit 766404262ce68c01c58c564bd1f9e27d00ea8cf3.

--- gym_tracker/app/src/main/java/com/chinhhuynh/gymtracker/HomeActivity.java
+++ gym_tracker/app/src/main/java/com/chinhhuynh/gymtracker/HomeActivity.java
@@ -2,10 +2,13 @@ package com.chinhhuynh.gymtracker;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.Arrays;
 import java.util.Date;
@@ -17,8 +20,7 @@ import com.chinhhuynh.gymtracker.model.Exercise;
 import com.chinhhuynh.gymtracker.model.ExerciseSummary;
 import com.chinhhuynh.gymtracker.tasks.ExtractAssetsTask;
 
-public class HomeActivity extends FragmentActivity
-        implements Loader.OnLoadCompleteListener<Cursor> {
+public class HomeActivity extends AppCompatActivity implements Loader.OnLoadCompleteListener<Cursor> {
 
     private RecyclerView mDailySummaries;
     private DailySummaryAdapter mAdapter;
@@ -34,6 +36,28 @@ public class HomeActivity extends FragmentActivity
         ExerciseLoader loader = new ExerciseLoader(this, "Sit up");
         loader.registerListener(0 /*id*/, this);
         loader.startLoading();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override

f2276451b89493757c2b67a0ade9d17049382365
Chinh Huynh <huynhphungchinh@gmail.com>
2016-01-31 16:07:09 -0800

    Add missing file.

--- gym_tracker/app/src/main/java/com/chinhhuynh/gymtracker/model/DailySummary.java
+++ gym_tracker/app/src/main/java/com/chinhhuynh/gymtracker/model/DailySummary.java
@@ -1,5 +1,7 @@
 package com.chinhhuynh.gymtracker.model;
 
+import android.text.format.DateUtils;
+
 import java.util.Date;
 import java.util.List;
 
@@ -11,5 +13,13 @@ public final class DailySummary {
     public DailySummary(Date date, List<ExerciseSummary> exercises) {
         mDate = date;
         mExercises = exercises;
+    }
+
+    public String getDateText() {
+        return DateUtils.getRelativeTimeSpanString(
+                mDate.getTime(),
+                System.currentTimeMillis(),
+                DateUtils.DAY_IN_MILLIS,
+                DateUtils.FORMAT_NUMERIC_DATE).toString();
     }
 }
